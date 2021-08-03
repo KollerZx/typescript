d7574872a646d8a9a0f9b73647ac48de22f7e4a6
Henrique Koller <38964774+KollerZx@users.noreply.github.com>
2021-08-03 13:43:11 -0300

    instalado typescript e express com tipagem, e configurado diretorio de compilação

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3333
+
+app.get('/', (req, res) => {
+  return res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running in http://localhost:${PORT}`)
+})
